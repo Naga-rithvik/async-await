e58e18a801b717b3a866d306ee2d4ed3c44d6078
Naga-rithvik <130950126+Naga-rithvik@users.noreply.github.com>
2023-05-18 09:24:32 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,46 @@
+const posts = [{ title: 'POST1' }, { title: 'POST2' }];
+
+function printPost() {
+    posts.forEach((post) => {
+        console.log(post.title);
+    });
+}
+
+function create3rdPost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push({ title: 'POST3' });
+            resolve();
+        }, 3000);
+    });
+}
+
+function create4thPost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push({ title: 'POST4' });
+            resolve();
+        }, 2000);
+    });
+}
+
+function deletePost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.pop();
+            resolve();
+        }, 1000);
+    });
+}
+
+async function processPosts() {
+    await create3rdPost();
+    await create4thPost();
+    await deletePost();
+    printPost();
+}
+
+(async () => {
+    await processPosts();
+})();
 
